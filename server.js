3be8ba11a8f3492a4d48d1ae87f088675b4ee25a
wjz20050714-stack <wjz20050714@gmail.com>
2026-01-21 11:39:05 +0800

    Change server log to always display port 3000

--- server.js
+++ server.js
@@ -587,5 +587,5 @@ app.get('/rooms', (req, res) => {
 });
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => {
-  console.log("Server is running on port " + PORT);
+  console.log("Server is running on port " + 3000);
 });
